c87d039165f388c94881170b08b529711f5a362a
nguyenthequyen210398 <nguyenthequyen.210398@gmail.com>
2024-06-07 19:54:12 +0700

    group3

---
+++ fe-book-movie-tickets/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import LogIn from './pages/auth/LogIn';
+
+function App() {
+ 
+
+  return (
+    <>
+      <LogIn />
+    </>
+  );
+}
+
+export default App;
