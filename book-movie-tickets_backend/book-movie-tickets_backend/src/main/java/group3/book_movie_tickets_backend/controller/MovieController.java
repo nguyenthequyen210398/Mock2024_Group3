7b2d242cbd988d2205dcf094afcfbdf2e1596eef
NamAst01 <145591149+NamAst01@users.noreply.github.com>
2024-06-11 15:31:25 +0700

    UPDATE Movie entity

--- book-movie-tickets_backend/book-movie-tickets_backend/src/main/java/group3/book_movie_tickets_backend/controller/MovieController.java
+++ book-movie-tickets_backend/book-movie-tickets_backend/src/main/java/group3/book_movie_tickets_backend/controller/MovieController.java
@@ -1,40 +1,51 @@
 package group3.book_movie_tickets_backend.controller;
 
-import group3.book_movie_tickets_backend.entity.Movie;
+
+import group3.book_movie_tickets_backend.dto.MovieDto;
+import group3.book_movie_tickets_backend.form.MovieCreateForm;
+import group3.book_movie_tickets_backend.form.MovieFilterForm;
 import group3.book_movie_tickets_backend.service.IMovieService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/movies")
 public class MovieController {
+
     @Autowired
-    private IMovieService iMovieService;
+    private IMovieService service;
 
     @GetMapping()
-    public ResponseEntity<?> findAll() {
-        return ResponseEntity.ok().body(iMovieService.getAll());
+    public Page<MovieDto> findAll(
+            MovieFilterForm form,
+            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir) {
+        Page<MovieDto> list = service.getAll(form, pageNo, pageSize, sortBy, sortDir);
+        return list;
     }
 
     @GetMapping(value = "/{id}")
-    public Movie findById(@PathVariable("id") Integer id) {
-        return iMovieService.findById(id);
+    public MovieDto findById(@PathVariable("id") Integer id) {
+        return service.getById(id);
     }
 
     @PostMapping
-    public void create(@RequestBody Movie form) {
-        iMovieService.create(form);
+    public void create(@RequestBody @Valid MovieCreateForm form) {
+        service.create(form);
     }
 
 
     @PutMapping("/{id}")
-    public void update(@PathVariable("id") Integer id, @RequestBody Movie form) {
-        iMovieService.updateById(id, form);
+    public void update(@PathVariable("id") Integer id, @RequestBody @Valid MovieDto form) {
+        service.updateById(id, form);
     }
 
     @DeleteMapping("/deleteById/{id}")
     public void deleteById(@PathVariable("id") Integer id) {
-        iMovieService.deleteById(id);
+        service.deleteById(id);
     }
 }
--- book-movie-tickets_backend/book-movie-tickets_backend/src/main/java/group3/book_movie_tickets_backend/repository/IMovieRepository.java
+++ book-movie-tickets_backend/book-movie-tickets_backend/src/main/java/group3/book_movie_tickets_backend/repository/IMovieRepository.java
@@ -2,6 +2,7 @@ package group3.book_movie_tickets_backend.repository;
 
 import group3.book_movie_tickets_backend.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface IMovieRepository extends JpaRepository<Movie, Integer> {
+public interface IMovieRepository extends JpaRepository<Movie, Integer>, JpaSpecificationExecutor<Movie> {
 }
