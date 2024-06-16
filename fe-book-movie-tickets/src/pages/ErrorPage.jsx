a6699ec35d33ab97de4dd25fae530758fb47ef84
nguyenthequyen210398 <nguyenthequyen.210398@gmail.com>
2024-06-16 17:14:49 +0700

    update

---
+++ fe-book-movie-tickets/src/pages/ErrorPage.jsx
@@ -0,0 +1,23 @@
+import './error.scss';
+
+export default function ErrorPage() {
+
+
+    return (
+        <>
+
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="main">
+                <h1>404</h1>
+                <p>Không tìm thấy trang...<br />Vui lòng thử lại sau.</p>
+                <a href="/index.html"><b>Trang chủ</b></a>
+            </div>
+
+
+        </>
+    );
+}
