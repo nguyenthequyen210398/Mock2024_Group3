cf9ff6d6904575b62993061450647d288f74a5a2
nguyenthequyen210398 <nguyenthequyen.210398@gmail.com>
2024-06-16 19:33:51 +0700

    update

--- fe-book-movie-tickets/src/pages/ErrorPage.jsx
+++ fe-book-movie-tickets/src/pages/ErrorPage.jsx
@@ -6,12 +6,12 @@ export default function ErrorPage() {
     return (
         <>
 
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="main">
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="main">
                 <h1>404</h1>
                 <p>Không tìm thấy trang...<br />Vui lòng thử lại sau.</p>
                 <a href="/index.html"><b>Trang chủ</b></a>
