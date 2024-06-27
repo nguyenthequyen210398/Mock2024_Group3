fe42b349948406fa2169d358835493eed5a01505
NamAst01 <145591149+NamAst01@users.noreply.github.com>
2024-06-27 19:56:27 +0700

    ADD UI of SignIn

--- fe-book-movie-tickets/src/App.jsx
+++ fe-book-movie-tickets/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import LogIn from './pages/auth/LogIn';
+import Login from './pages/auth/Login.jsx';
 
 function App() {
  
 
   return (
     <>
-      <LogIn />
+      <Login />
     </>
   );
 }
