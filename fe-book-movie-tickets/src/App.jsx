34fb830fe6fbd9229a544abb36cd6302c5ca03ad
nguyenthequyen210398 <110225836+nguyenthequyen210398@users.noreply.github.com>
2024-07-02 21:21:12 +0700

    update phân trang

--- fe-book-movie-tickets/src/App.jsx
+++ fe-book-movie-tickets/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       {/* <Footer /> */}
       {/* <Header /> */}
       <Movie />
+      {/* <TicketMana /> */}
 
     </>
   );
--- fe-book-movie-tickets/src/components/MovieIsShowing.jsx
+++ fe-book-movie-tickets/src/components/MovieIsShowing.jsx
@@ -1,24 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useGetListDataAPI } from "../api/cinemaApi";
 import { toDay } from "../utils/date";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
 
 function MovieIsShowing() {
     const [movies, setMovies] = useGetListDataAPI(`https://66794dd518a459f6394f1eec.mockapi.io/cinema`);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(6);
+
     const movieComing = movies.filter(item => item.releaseDate <= toDay);
 
     const handleWatchTrailer = (trailerUrl) => {
         window.open(trailerUrl, '_blank'); // Open YouTube trailer in a new window/tab
     };
 
-
+    // Logic for pagination
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = movieComing.slice(indexOfFirstItem, indexOfLastItem);
+    // Change page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
     return (
         <>
             <div className="movie-showing">
                 <h2>Phim Đang Chiếu</h2>
 
                 <div className="movie-list">
-                    {movieComing.map((movie, index) => (
+                    {currentItems.map((movie, index) => (
                         <div key={index} className="movie-item">
                             <img src={movie.imageUrl} alt={movie.title} />
                             <p className="movie-title">{movie.title}</p>
@@ -29,6 +38,12 @@ function MovieIsShowing() {
                             </div>
                         </div>
                     ))}
+                </div>
+
+                {/* Pagination buttons */}
+                <div className="pagination">
+                    <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
+                    <button onClick={() => paginate(currentPage + 1)} disabled={currentItems.length < itemsPerPage || indexOfLastItem >= movieComing.length}>Next</button>
                 </div>
             </div>
         </>
